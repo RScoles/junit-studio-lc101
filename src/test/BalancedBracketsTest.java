c2e2ab228cc11be7173272544cd03e01e04548dd
Carly Langlois <cj.langlois.cl@gmail.com>
2021-04-20 17:42:57 -0500

    removing .idea from git

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,10 +1,12 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+
 
     //TODO: add tests here
     @Test
@@ -12,5 +14,62 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void twoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+
+    @Test
+    public void bracketsWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void bracketsOfBracketsWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]][]"));
+    }
+
+    @Test
+    public void tripleBracketsWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+
+    @Test
+    public void twoTripleBracketsWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[][]]]"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void bracketsWithStringBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[String]"));
+    }
+
+    @Test
+    public void oneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void misMatchedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+
+    }
+
+    @Test
+    public void unequalBracketsWithStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Sting[]"));
+    }
 }
+
+
+
